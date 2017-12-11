a5cea11a078f771ed97abf75496be0bf4165bcb3
Python School <school@python.com>
2017-12-11 20:46:22 +0100

    some changes after review

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-import requests
-import json
 import argparse
 from datetime import datetime
+import requests
+import json
 
 class JenkinsFinder(object):
     def __init__(self, url, job):
@@ -30,11 +30,11 @@ class JenkinsFinder(object):
         dt_obj = datetime.strptime(date, '%Y-%m-%d %H:%M')
         list_of_jsons = self.url_parser()
         artifacts_list = []
-        for data in list_of_jsons:
-            if dt_obj < datetime.fromtimestamp(data["timestamp"]/1000):
-                for artifact in data["artifacts"]:
-                    artifacts_list.append(self.url+'/job/'+self.job+'/lastSuccessfulBuild/artifact/'+artifact["fileName"])
-        print(artifacts_list)
+        for single_json in list_of_jsons:
+            if dt_obj < datetime.fromtimestamp(single_json["timestamp"]/1000):
+                for artifact in single_json["artifacts"]:
+                    artifacts_list.append(self.url+'/job/'+self.job+
+                            '/lastSuccessfulBuild/artifact/'+artifact["fileName"])
         return artifacts_list
 
 def main():
