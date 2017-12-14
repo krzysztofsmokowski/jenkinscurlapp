6907c39160616869e26e972c6ba74b96f3dd2682
Python School <school@python.com>
2017-12-15 00:50:43 +0100

    added unittests for builds_urls method; minor refactor WIP

--- app.py
+++ app.py
@@ -5,51 +5,55 @@ import json
 
 class JenkinsFinder(object):
     def __init__(self, url, job):
-        self.job = job
+        self.jobname = job
         self.url = url
-        self.request = requests.get(url+'/job/'+job+'/api/json').json()
-        self.list_of_jsons = []
-        self.url_list = []
+        self.builds = []
 
-    def builds_urls(self, num=100):
-        if self.request['nextBuildNumber'] <= num:
-            for build in self.request["builds"]:
-                self.url_list.append(build["url"])
+    def _api_call(self, url):
+        return requests.get(url).json()
+
+    def builds_urls(self, num=10):
+        urls = []
+        job = self._api_call(self.url+'/job/'+self.jobname+'/api/json')
+        if job['nextBuildNumber'] <= num:
+            for build in job["builds"]:
+                urls.append(build["url"])
         else:
-            for build in self.request["builds"]:
-                self.url_list.append(build["url"][:num])
-        return self.url_list
+            for build in job["builds"][:num]:
+                urls.append(build["url"])
+        print(urls)
+        return urls
 
     def url_parser(self):
         url_list = self.builds_urls()
         for url in url_list:
-            self.list_of_jsons.append(requests.get(url + '/api/json').json())
-        return self.list_of_jsons
+            self.builds.append(requests.get(url + '/api/json').json())
+        return self.builds
 
     def artifact_printer(self, date):
         dt_obj = datetime.strptime(date, '%Y-%m-%d %H:%M')
-        list_of_jsons = self.url_parser()
+        builds = self.url_parser()
         artifacts_list = []
-        for single_json in list_of_jsons:
+        for single_json in builds:
             if dt_obj < datetime.fromtimestamp(single_json["timestamp"]/1000):
                 for artifact in single_json["artifacts"]:
-                    artifacts_list.append(self.url+'/job/'+self.job+
+                    artifacts_list.append(self.url+'/job/'+self.jobname+
                             '/lastSuccessfulBuild/artifact/'+artifact["fileName"])
         return artifacts_list
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--url", help = 'setting url for parsing')
+    parser.add_argument("--url", help = 'setting url for parsing', required = True)
     parser.add_argument("--builds", action = "store_true", help = 'printing urls of builds')
     parser.add_argument("--artifact_printer", action = "store_true", help = 'printing artifacts before specified date')
-    parser.add_argument("--date", help = 'specifying date')
-    parser.add_argument("--job", help = 'specifying job of jenkins CI')
+    parser.add_argument("--date", help = 'specifying date in format Year-month-day Hour:Minute')
+    parser.add_argument("--job", help = 'specifying job of jenkins CI', required = True)
     args = parser.parse_args()
     jenkins = JenkinsFinder(args.url, args.job)
     if args.builds:
-        what_to_print = jenkins.builds_urls()
+        jenkins.builds_urls()
     if args.artifact_printer and args.date:
-        what_to_print = jenkins.artifact_printer(args.date)
+        jenkins.artifact_printer(args.date)
 
 
 if __name__ == "__main__":
